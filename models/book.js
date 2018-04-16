57d3c4e6ae6d62e30a8dd0ea49d1013f491e0aec
RhiaKe <rhiakaulitzke@gmail.com>
2018-04-16 15:20:33 -0600

    updated git

---
+++ models/book.js
@@ -0,0 +1,13 @@
+// book.js
+var mongoose = require('mongoose');
+
+var BookSchema = new mongoose.Schema({
+     title: String,
+     author: String,
+     image: String,
+     release_date: String
+ });
+
+var Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
